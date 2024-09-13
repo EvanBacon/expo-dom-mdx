87e36dea37c8339a3575ca3e144e067fbb3ba868
evanbacon <bacon@expo.io>
2024-09-13 14:23:34 +0100

    fix web support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,5 @@
-import { Stack } from "expo-router";
-import { PlatformColor } from "react-native";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerTransparent: true,
-        headerLargeTitle: true,
-        headerBlurEffect: "prominent",
-        headerShadowVisible: true,
-        headerLargeTitleShadowVisible: false,
-        headerStyle: {
-          // Hack to ensure the collapsed small header shows the shadow / border.
-          backgroundColor: "rgba(255,255,255,0.01)",
-        },
-        headerLargeStyle: {
-          backgroundColor: PlatformColor("systemGroupedBackgroundColor"), // Color of your background
-        },
-
-        title: "Hello",
-      }}
-    />
-  );
+  return <Slot />;
 }
