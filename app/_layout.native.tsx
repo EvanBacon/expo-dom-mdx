0879b7b3eb3109994eabbda07dba0f22d7c77761
evanbacon <bacon@expo.io>
2024-09-20 15:54:00 +0200

    add arc haptics scrolling

--- app/_layout.native.tsx
+++ app/_layout.native.tsx
@@ -5,19 +5,19 @@ export default function RootLayout() {
   return (
     <Stack
       screenOptions={{
-        headerTransparent: true,
-        headerLargeTitle: true,
-        headerBlurEffect: "prominent",
-        headerShadowVisible: true,
-        headerLargeTitleShadowVisible: false,
-        headerStyle: {
-          // Hack to ensure the collapsed small header shows the shadow / border.
-          backgroundColor: "rgba(255,255,255,0.01)",
-        },
-        // @ts-expect-error
-        headerLargeStyle: {
-          backgroundColor: PlatformColor("systemGroupedBackgroundColor"), // Color of your background
-        },
+        // headerTransparent: true,
+        // headerLargeTitle: true,
+        // headerBlurEffect: "prominent",
+        // headerShadowVisible: true,
+        // headerLargeTitleShadowVisible: false,
+        // headerStyle: {
+        //   // Hack to ensure the collapsed small header shows the shadow / border.
+        //   backgroundColor: "rgba(255,255,255,0.01)",
+        // },
+        // // @ts-expect-error
+        // headerLargeStyle: {
+        //   backgroundColor: PlatformColor("systemGroupedBackgroundColor"), // Color of your background
+        // },
 
         title: "Hello",
       }}
